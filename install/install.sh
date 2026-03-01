0079c79912a7c90a2c352d03dd6f5af8f5b226b3
karunix <karunix@protonmail.com>
2026-03-01 15:42:00 +0700

    Enhance install script to create global symlinks for core tools

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,7 @@
 set -e
 
 INSTALL_DIR="/opt/linux-bash-tools"
+BIN_DIR="/usr/local/bin"
 
 echo "[*] Creating install directory..."
 sudo mkdir -p "$INSTALL_DIR"
@@ -12,5 +13,11 @@ sudo cp -r ../tools "$INSTALL_DIR/"
 
 echo "[*] Setting executable permissions..."
 sudo find "$INSTALL_DIR" -type f -name "*.sh" -exec chmod +x {} \;
+
+echo "[*] Creating symlinks..."
+for script in $(find "$INSTALL_DIR/tools/core" -type f -name "*.sh"); do
+    name=$(basename "$script" .sh)
+    sudo ln -sf "$script" "$BIN_DIR/$name"
+done
 
 echo "[+] Installation complete."
